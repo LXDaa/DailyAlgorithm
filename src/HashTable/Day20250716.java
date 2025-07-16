5e0fa007ed34ac4933fd05e4d26665b2875c44e3
LXDa <2439185947@qq.com>
2025-07-16 19:02:01 +0800

    202. 快乐数 1. 两数之和

---
+++ src/HashTable/Day20250716.java
@@ -0,0 +1,54 @@
+package HashTable;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Day20250716 {
+    /**
+     * 202. 快乐数
+     * 编写一个算法来判断一个数 n 是不是快乐数。
+     * <p>
+     * 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+     * <p>
+     * 如果 n 是快乐数就返回 True ；不是，则返回 False 。
+     */
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    private int getNext(int n) {
+        int result = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            n = n / 10;
+            result += temp * temp;
+        }
+        return result;
+    }
+
+    /**
+     * 1. 两数之和
+     * <p>
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
