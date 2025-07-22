40c5f403bd90dde854be7bcb33501bdfd2105d1b
LXDa <2439185947@qq.com>
2025-07-22 19:47:34 +0800

    344. 反转字符串 & 541. 反转字符串II

---
+++ src/String/Day20250722.java
@@ -0,0 +1,42 @@
+package String;
+
+import java.util.Scanner;
+
+public class Day20250722 {
+    /**
+     * 344. 反转字符串
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+     */
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left++] = s[right];
+            s[right--] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    /**
+     * 541. 反转字符串II
+     * 给定一个字符串 s 和一个整数 k，从字符串开头算起, 每计数至 2k 个字符，就反转这 2k 个字符中的前 k 个字符。
+     * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+     * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+     */
+    public String reverseStr(String s, int k) {
+        char[] array = s.toCharArray();
+        for (int i = 0; i < array.length; i += 2 * k) {
+            int start = i;
+            int end = Math.min(start + k - 1, array.length - 1);
+            while (start < end) {
+                char temp = array[start];
+                array[start++] = array[end];
+                array[end--] = temp;
+            }
+        }
+        return new String(array);
+    }
+}
