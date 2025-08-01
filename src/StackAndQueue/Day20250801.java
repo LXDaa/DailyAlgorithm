22c337f7fa6547b6f35f2884e1c17be434df6c92
LXDa <2439185947@qq.com>
2025-08-01 20:38:31 +0800

    20. 有效的括号

---
+++ src/StackAndQueue/Day20250801.java
@@ -0,0 +1,36 @@
+package StackAndQueue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 20. 有效的括号
+ * <p>
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 1. 左括号必须用相同类型的右括号闭合。
+ * 2. 左括号必须以正确的顺序闭合。
+ * 3. 每个右括号都有一个对应的相同类型的左括号。
+ */
+public class Day20250801 {
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                deque.push(')');
+            } else if (c == '[') {
+                deque.push(']');
+            } else if (c == '{') {
+                deque.push('}');
+            } else if (deque.isEmpty() || deque.peek() != c) {
+                return false;
+            } else {
+                deque.pop();
+            }
+        }
+        return deque.isEmpty();
+    }
+}
