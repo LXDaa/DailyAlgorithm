49355afe6aca9059781b053b56d3414e45314833
LXDa <2439185947@qq.com>
2025-08-14 21:24:20 +0800

    102.二叉树的层序遍历

--- src/Tree/Day20250811.java
+++ src/Tree/Day20250811.java
@@ -1,8 +1,7 @@
 package Tree;
 
 import javax.swing.tree.TreeNode;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 144.二叉树的前序遍历
@@ -74,5 +73,75 @@ public class Day20250811 {
         postorder(root.left, res);
         postorder(root.right, res);
         res.add(root.val);
+    }
+
+    /**
+     * 迭代前序遍历
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 迭代中序遍历
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                TreeNode node = stack.pop();
+                res.add(node.val);
+                cur = node.right;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 迭代后序遍历
+     */
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        Collections.reverse(res);
+        return res;
     }
 }
