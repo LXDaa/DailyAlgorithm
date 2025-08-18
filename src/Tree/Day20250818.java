a1272f012e69e1614d7c51d0ce011a8eab6871ba
LXDa <2439185947@qq.com>
2025-08-18 20:04:28 +0800

    101. 对称二叉树

---
+++ src/Tree/Day20250818.java
@@ -0,0 +1,72 @@
+package Tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 101. 对称二叉树
+ * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ */
+public class Day20250818 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //递归
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return compare(root.left, root.right);
+    }
+
+    private boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return compare(left.left, right.right) && compare(left.right, right.left);
+    }
+
+    //迭代
+    public boolean isSymmetric2(TreeNode root) {
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offerFirst(root.left);
+        deque.offerLast(root.right);
+        while (!deque.isEmpty()) {
+            TreeNode left = deque.pollFirst();
+            TreeNode right = deque.pollLast();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null || left.val != right.val) {
+                return false;
+            }
+            deque.offerFirst(left.left);
+            deque.offerFirst(left.right);
+            deque.offerFirst(right.right);
+            deque.offerFirst(right.left);
+
+//            deque.offerLast(right.right);
+//            deque.offerLast(right.left);
+        }
+        return true;
+    }
+}
